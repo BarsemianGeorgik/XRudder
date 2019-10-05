e06a847368d307fd175e8962b48ca47b66cedfce
georgikbarsemian <georgik.barsemian@mail.concordia.ca>
2019-10-05 19:29:31 -0400

    Fixing the move bug from infinite loop and adding a function to determine if a player won the game

--- GameBoard.py
+++ GameBoard.py
@@ -15,6 +15,19 @@ class GameBoard:
         self.x_val = 0
         self.y_val = 0
 
+    def didPlayerTokenWin(self, playerToken, adversaryToken):
+        gameOver = False
+        for x in range(1, 9):
+            for j in range(1, 11):
+                if playerToken == self.board[x][j]:
+                    # check top left , top right, bottom left and bottom right
+                    if ((playerToken == self.board[x-1][j-1]) & (playerToken == self.board[x-1][j+1])
+                            & (playerToken == self.board[x+1][j-1]) & (playerToken == self.board[x+1][j+1])):
+                        # Check if opponent has blocked the player X
+                        if(adversaryToken != self.board[x][j-1]) & (adversaryToken != self.board[x][j+1]):
+                            gameOver = True
+        return gameOver
+
     def printBoard(self):
         alphabetAxis = ['\t', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
         print("============= GAME STATUS ============= ")
@@ -34,49 +47,53 @@ class GameBoard:
         # Check if available
         # If assigned, ask to enter again
         # If available, assign index to player token
-        self.stringToIndex(args)
-        if self.board[self.x_val][self.y_val] == '□':
-            print("Placing the token")
-            self.board[self.x_val][self.y_val] = playerToken
-            self.printBoard()
-            return True
+        valid = self.stringToIndex(args)
+        if valid:
+            if self.board[self.x_val][self.y_val] == '□':
+                print("Placing the token")
+                self.board[self.x_val][self.y_val] = playerToken
+                return True
 
-        if self.board[self.x_val][self.y_val] != '□':
-            print("That location is already taken")
+            else:
+                print("Invalid location")
+                return False
+        else:
             return False
 
     def moveToken(self, args, playerToken):
         print("Moving the token")
-        # Check if token exists at location and is assigned to player
-        self.stringToIndex(args)
-        if self.board[self.x_val][self.y_val] == playerToken:
-            prevX = self.x_val
-            prevY = self.y_val
-            indexMove = input("Enter the index which you'd like to move to (Letter followed by number):").upper()
+        # Check if valid input
+        valid = self.stringToIndex(args)
+        if valid:
+            # check if token exists
+            if self.board[self.x_val][self.y_val] == playerToken:
+                prevX = self.x_val
+                prevY = self.y_val
 
-            while self.putToken(indexMove, playerToken) == False:
-                indexMove = input("Enter the index which you'd like to move to (Letter followed by number):").upper()
-                self.putToken(indexMove, playerToken)
-            print(prevX)
-            print(prevY)
-            self.board[prevX][prevY] = '□'
+                validPutAction = False
+                while not validPutAction:
+                    indexMove = input("Enter the index which you'd like to move to (Letter followed by number):").upper()
+                    validPutAction = self.putToken(indexMove, playerToken)
+                print("previous slots: "+str(prevX) + " " + str(prevY))
+                self.board[prevX][prevY] = '□'
+                return True
 
-            return True
-
-        if self.board[self.x_val][self.y_val] != playerToken:
-            print("That field does not contain your token. Please try again")
+            elif self.board[self.x_val][self.y_val] != playerToken:
+                print("That field does not contain your token. Please try again")
+                return False
+        else:
             return False
-        # If not, ask to enter again
-        # Ask the position they'd like to move to
-        # call putToken function
+
 
     def stringToIndex(self, input):
         # check if within a to m, and 1 to 10
         input.replace(" ", "")
         if len(input) > 3 or input[0].upper() not in "ABCDEFGHIJKL" or int(input[1:]) > 10:
             print("That is an invalid input")
+            return False
         else:
             self.y_val = (ord(input[0].lower()) - 97)  # -97 to get the index.. a-97 = 0, etc.
             self.x_val = 10 - int(input[1:])
-
+            print("valid index")
             print(self.x_val, self.y_val)
+            return True
