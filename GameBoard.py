76b0c61dd7076f6ddb0082c8af7fad965ffc94c8
Ayah <abadran96@gmail.com>
2019-11-10 20:48:19 -0500

    spaghetti code

--- GameBoard.py
+++ GameBoard.py
@@ -1,3 +1,106 @@
+import copy
+
+
+def printTempBoard(boardList):
+    alphabetAxis = ['\t', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
+    print("============= GAME STATUS ============= ")
+    rowNumber = 10
+    for row in boardList:
+        print(str(rowNumber), end='\t ')
+        for elem in row:
+            print(elem, end=' ')
+        rowNumber = rowNumber - 1
+        print()
+
+    for x in alphabetAxis:
+        print(x, end=' ')
+    print()
+    print()
+
+
+def allPutOptions(player, board):
+    boards = []
+    tempboard = copy.deepcopy(board)
+
+    for (i, row) in enumerate(board):
+        for (j, value) in enumerate(row):
+            if value == ".":
+                print("I HERE HERE HERE HERE HERE AS MANY TIMES AS I AM MAKING A NEW BOARD FOR MY OPTIONS ")
+                value = player.tokenCharacter
+                tempboard[i][j] = player.tokenCharacter
+                boards.append(copy.deepcopy(tempboard))  # create new board of the temporary one
+                tempboard[i][j] = "."
+                # print(i, j, value)
+    for x in boards:
+        printTempBoard(x)
+
+    return boards
+
+
+def spaghettistringToIndex(userInput):
+    # check if within a to m, and 1 to 10
+    userInput.replace(" ", "")
+    if len(userInput) > 3 or userInput[0].upper() not in "ABCDEFGHIJKL" or int(userInput[1:]) > 10:
+        print("That is an invalid input")
+        return False
+    else:
+        y = (ord(userInput[0].lower()) - 97)  # -97 to get the index.. a-97 = 0, etc.
+        x = 10 - int(userInput[1:])
+
+        coordinate = str(y) + str(x)
+        # print("valid index")
+        # print(self.x_val, self.y_val)
+        return coordinate
+
+
+def possibleMoves(player, board):
+    boards = []  # possible boards
+    #   function that calculates the possible moves of a player
+    #   takes list of player indexes, for every index make a list of boards
+    for index in player.playerTokenLocations:
+        indexof = spaghettistringToIndex(index)
+        prevX = int(indexof[0])
+        prevY = int(indexof[1])
+
+        possibleIndex = []  # possible indexes of the new locations from given index
+
+        boardIndex = 0
+        # Add a new board to the list
+        newBoard = copy.deepcopy(board)
+        for x in range(-1, 2):
+            for y in range(-1, 2):
+                newX = chr(ord(index[0]) + x)
+                newY = str(int(index[1:]) + y)  # done this way to handle index 10
+                if newX in "ABCDEFGHIJKL" and 0 < int(newY) <= 10:
+                    newXY = newX + newY
+
+                    # check if this place is occupied
+                    valid = spaghettistringToIndex(newXY)
+                    newX = int(indexof[0])
+                    newY = int(indexof[1])
+                    if valid:
+                        if board[newX][newY] == '.':
+                            possibleIndex.append(newXY)
+
+                            newBoard[newX][newY] = player.tokenCharacter
+                            newBoard[prevX][prevY] = '.'
+
+                            boards.append(copy.deepcopy(newBoard))
+
+                            newBoard[newX][newY] = '.'
+
+                            print("This is board #" + str(boardIndex))
+                            printTempBoard(boards[boardIndex])
+                            boardIndex = boardIndex + 1
+
+        print(possibleIndex)
+        print("THESE ARE MY BOARDS")
+        for each in boards:
+            print(each)
+
+    return boards
+
+
 class GameBoard:
 
     def __init__(self):
@@ -47,7 +150,7 @@ class GameBoard:
         print()
         print()
 
-    def putToken(self, args, playerToken):
+    def putToken(self, args, player):
         # Check if available
         # If assigned, ask to enter again
         # If available, assign index to player token
@@ -55,7 +158,8 @@ class GameBoard:
         if valid:
             if self.board[self.x_val][self.y_val] == '.':
                 print("Placing the token")
-                self.board[self.x_val][self.y_val] = playerToken
+                self.board[self.x_val][self.y_val] = player.tokenCharacter
+                player.playerTokenLocations.append(args)
                 return True
 
             else:
@@ -64,12 +168,12 @@ class GameBoard:
         else:
             return False
 
-    def moveToken(self, args, playerToken):
+    def moveToken(self, args, player):
         # Check if valid input
         valid = self.stringToIndex(args)
         if valid:
             # check if token exists
-            if self.board[self.x_val][self.y_val] == playerToken:
+            if self.board[self.x_val][self.y_val] == player.tokenCharacter:
                 prevX = self.x_val
                 prevY = self.y_val
 
@@ -82,25 +186,27 @@ class GameBoard:
                     self.stringToIndex(indexMove)
                     # Check if the move is of only one space
                     x_delta = self.x_val - prevX
-                   # print(str(x_delta) + " x difference")
+                    # print(str(x_delta) + " x difference")
                     y_delta = self.y_val - prevY
-                  #  print(str(y_delta) + " y difference")
+                    #  print(str(y_delta) + " y difference")
 
                     if -1 <= x_delta <= 1 and -1 <= y_delta <= 1:
                         validOneSpace = True
                         print("That's a valid one space move")
 
                         if validOneSpace:
-                            validPutAction = self.putToken(indexMove, playerToken)
+                            validPutAction = self.putToken(indexMove, player)
 
                     elif not validOneSpace:
                         print("That is not a valid one space move. Try again. ")
                 if validPutAction:
                     self.board[prevX][prevY] = '.'
                     print("Moving the token")
+                    print(args.upper() + " is being removed from the player's list.")
+                    player.playerTokenLocations.remove(args.upper())
                     return True
 
-            elif self.board[self.x_val][self.y_val] != playerToken:
+            elif self.board[self.x_val][self.y_val] != player.tokenCharacter:
                 print("That field does not contain your token. Please try again")
                 return False
         else:
@@ -115,6 +221,8 @@ class GameBoard:
         else:
             self.y_val = (ord(userInput[0].lower()) - 97)  # -97 to get the index.. a-97 = 0, etc.
             self.x_val = 10 - int(userInput[1:])
-            print("valid index")
-           # print(self.x_val, self.y_val)
+            # print("valid index")
+            # print(self.x_val, self.y_val)
             return True
+
+    # return new boards for the put option
--- GameDriver.py
+++ GameDriver.py
@@ -1,5 +1,7 @@
-from GameBoard import GameBoard
+from GameBoard import GameBoard, allPutOptions, possibleMoves
 from Player import Player
+from MiniMaxNode import MiniMaxNode
+from MiniMaxTree import MiniMaxTree
 
 if __name__ == '__main__':
 
@@ -7,7 +9,7 @@ if __name__ == '__main__':
     turnCounter = 0
     movesActionsRemaining = 30
 
-    #Player  initilization
+    # Player  initialization
     p1 = Player("Player 1", "X")
     p2 = Player("Player 2", "O")
 
@@ -15,12 +17,25 @@ if __name__ == '__main__':
     player = " "
     gameBoard = GameBoard()
 
-    print("The Game has Started!")
-    gameBoard.printBoard()
     player1Win = False
     player2Win = False
+    aIEnabled = False
 
-    while (not player1Win) & (not player2Win) & ((movesActionsRemaining != 0) & (p1.tokensRemaining != 0) & (p2.tokensRemaining != 0)) :
+    while True:
+        enablingAI = input(
+            "Would you wanna play against an AI? [Y]es or [N]o?").upper()
+        if enablingAI not in "YN" or len(enablingAI) != 1:
+            print("The entry was not correct please try again")
+            continue
+        break;
+
+    if enablingAI == 'Y':
+        aIEnabled = True
+
+    print("The Game has Started!")
+    gameBoard.printBoard()
+    while (not player1Win) & (not player2Win) & (
+            (movesActionsRemaining != 0) & (p1.tokensRemaining != 0) & (p2.tokensRemaining != 0)):
         turnCounter = turnCounter + 1
 
         # Choosing the Player's turn
@@ -31,28 +46,31 @@ if __name__ == '__main__':
             player = p2
             isFirstPlayerTurn = True
 
-        # Player action input
+        # checking if its AI turn if its enabled
+        action = 'A'
         player.printPlayerStatus()
-        while True:
-            if turnCounter <= 2:
-                print(player.name + " Place your first token!")
-                action = 'P'
-            elif player.tokensRemaining == 0:
-                print(player.name + " has run out of tokens. You can only move existing tokens.")
-                action = 'M'
-            elif movesActionsRemaining == 0 and player.tokensRemaining != 0:
-                print("Players have run out of move actions. You can only put new tokens on the board. ")
-                action = 'P'
-            elif movesActionsRemaining == 0 and player.tokensRemaining == 0:
-                print(player.name + " has no remaining tokens or moves. Turn ending.")
+        if (player != p2) or (not aIEnabled):
+            # Player action input
+            while True:
+                if turnCounter <= 2:
+                    print(player.name + " Place your first token!")
+                    action = 'P'
+                elif player.tokensRemaining == 0:
+                    print(player.name + " has run out of tokens. You can only move existing tokens.")
+                    action = 'M'
+                elif movesActionsRemaining == 0 and player.tokensRemaining != 0:
+                    print("Players have run out of move actions. You can only put new tokens on the board. ")
+                    action = 'P'
+                elif movesActionsRemaining == 0 and player.tokensRemaining == 0:
+                    print(player.name + " has no remaining tokens or moves. Turn ending.")
 
-            else:
-                action = input(
-                    "It's " + player.name + "'s turn! What would you like to do? [P]ut a new token or [M]ove an existing one?").upper()
-            if action not in "PM" or len(action) != 1:
-                print("The entry was not correct please try again")
-                continue
-            break
+                else:
+                    action = input(
+                        "It's " + player.name + "'s turn! What would you like to do? [P]ut a new token or [M]ove an existing one?").upper()
+                if action not in "PM" or len(action) != 1:
+                    print("The entry was not correct please try again")
+                    continue
+                break
 
         # Action Process
 
@@ -62,12 +80,10 @@ if __name__ == '__main__':
             validPutAction = False
             while not validPutAction:
                 indexMove = input("Enter the index of your put action (Letter followed by number):").upper()
-                validPutAction = gameBoard.putToken(indexMove, player.tokenCharacter)
+                validPutAction = gameBoard.putToken(indexMove, player)
                 if validPutAction:
-                    #print("Removing one token from total")
+                    # print("Removing one token from total")
                     player.tokensRemaining = player.tokensRemaining - 1
-                    player.printPlayerStatus()
-
 
         elif action == 'M':
             # implementation for Moving a token
@@ -75,10 +91,66 @@ if __name__ == '__main__':
             validMoveAction = False
             while not validMoveAction:
                 indexMove = input("Enter the index of your move (Letter followed by number):").upper()
-                validMoveAction = gameBoard.moveToken(indexMove, player.tokenCharacter)
-                movesActionsRemaining - 1
+                validMoveAction = gameBoard.moveToken(indexMove, player)
+                movesActionsRemaining = movesActionsRemaining - 1
+                player.printPlayerTokens()
+
+        elif action == 'A':
+            print("AI is calculating it's move")
+            miniMaxTree = MiniMaxTree()
+            AI_moves = []
+            AI_nodes = []
+            player_moves = []  # list of lists of player moves
+            player_nodes = []  # list of lists of player nodes
+            temp = []
+            rootnode = MiniMaxNode(gameBoard.board, 0)  # create node of the current gameboard
+            miniMaxTree.setRoot(rootnode)  # set as the root node
+            if p1.tokensRemaining > 0:
+                AI_moves.extend(allPutOptions(p1, gameBoard.board))  # calculate all put options for AI
+            if movesActionsRemaining > 0:
+                AI_moves.extend(possibleMoves(p1, gameBoard.board))  # calculate all move options for AI
+
+            for each in AI_moves:  # make each AI move a node
+                AI_nodes.append(MiniMaxNode(each, 0))  # setting heuristic to 0 for all of them,
+
+            for each in AI_moves:  # calculate player moves for each AI move
+                moves = []
+                if p2.tokensRemaining > 0:
+                    moves = allPutOptions(p2, each)
+
+                if movesActionsRemaining > 0:
+                    moves = possibleMoves(p2, each)
+                    print(moves)
+                    moves.extend(possibleMoves(p2, each))
+                    player_moves.append(moves)
+
+                for move in moves:
+                    minimaxnode = MiniMaxNode(move, 0)
+                    minimaxnode.calculateHeuristic()
+                    temp.append(MiniMaxNode(move, 0))  # make playernode
+                    #  call heuristic function on the playermoves when created (can make it in the node class itself)
+                player_nodes.append(temp)
+                moves = []  # clear player move array
+                temp = []
+            i = 0
+            #  iterate through
+            for each in AI_nodes:
+                each.setChildren(player_nodes[i])
+                i = i + 1
+            rootnode.setChildren(AI_nodes)
+            gameBoard = miniMaxTree
+            # creating all AI moves
+            # creating all player moves followed by AI
+            # give a heuristic to each of those player's move
+            # Add player nodes to it's parent AI node
+            # Add all AI nodes to the root Node
+            # set the root node to the minimaxTree
+            # gameBoard = miniMaxTree.getAIComputedMove()
 
         # Print the Board
+        player.printPlayerStatus()
+        player.printPlayerTokens()
+        print("Moves remaining: " + str(movesActionsRemaining))
         gameBoard.printBoard()
 
         # Check if a player won
@@ -90,4 +162,5 @@ if __name__ == '__main__':
     elif player2Win:
         print("Player 2 won the game")
     else:
+
         print("Game Over: End of round")
