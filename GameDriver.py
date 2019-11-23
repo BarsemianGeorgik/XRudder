981228efbd24dfe9eeb5e02b88ce03ded57bcdb1
Monique <monique.richa@gmail.com>
2019-11-22 19:46:17 -0500

    Heurisitic function now properly blocks an opponent

--- GameDriver.py
+++ GameDriver.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
 
                     # find out which token the AI has
                     # 
-                    minimaxnode.newheuristic('O', 'X')  # call heuristic function on leaf nodes
+                    minimaxnode.finalheuristic('O', 'X')  # call heuristic function on leaf nodes
                     temp.append(copy.deepcopy(minimaxnode))  # add to array of player nodes for specific AI move
 
                 player_nodes.append(temp)
